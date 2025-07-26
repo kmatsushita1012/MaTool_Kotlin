5b6655a1de919c4151849ab7d13c707ba69a4579
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-07-26 22:13:42 +0900

    contract作成

--- app/src/main/java/com/studiomk/matool/domain/contracts/auth/AuthProvider.kt
+++ app/src/main/java/com/studiomk/matool/domain/contracts/auth/AuthProvider.kt
@@ -9,6 +9,11 @@ interface AuthProvider {
     suspend fun getUserRole(): Result<UserRole, AuthError>
     suspend fun getTokens(): Result<String?, AuthError>
     suspend fun signOut(): Result<Boolean, AuthError>
+    suspend fun changePassword(current: String, new: String): Result<Unit, AuthError>
+    suspend fun resetPassword(username: String): Result<Unit, AuthError>
+    suspend fun confirmResetPassword(username: String, newPassword: String, code: String): Result<Unit, AuthError>
+    suspend fun updateEmail(newEmail: String): UpdateEmailResult
+    suspend fun confirmUpdateEmail(code: String): Result<Unit, AuthError>
 }
 
 
