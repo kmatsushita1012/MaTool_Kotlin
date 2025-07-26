be13cb0ebb559ac9ce5825ac4793f1d7dadbbc13
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-07-26 22:19:40 +0900

    Service実装

--- app/src/main/java/com/studiomk/matool/application/service/AuthService.kt
+++ app/src/main/java/com/studiomk/matool/application/service/AuthService.kt
@@ -5,6 +5,7 @@ import SignInResult
 import com.studiomk.matool.domain.contracts.auth.AuthError
 import com.studiomk.matool.domain.contracts.auth.AuthProvider
 import com.studiomk.matool.domain.contracts.auth.SignInResponse
+import com.studiomk.matool.domain.contracts.auth.UpdateEmailResult
 import com.studiomk.matool.domain.entities.shared.*
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
@@ -78,6 +79,34 @@ class AuthService : KoinComponent {
             is Result.Success -> tokenResult.value
             is Result.Failure -> null
         }
+    }
+
+    suspend fun changePassword(current: String, new: String): Result<Unit, AuthError> = mutex.withLock {
+        authProvider.changePassword(current, new)
+    }
+
+    suspend fun resetPassword(username: String): Result<Unit, AuthError> = mutex.withLock {
+        authProvider.resetPassword(username)
+    }
+
+    suspend fun confirmResetPassword(username: String, newPassword: String, code: String): Result<Unit, AuthError> = mutex.withLock {
+        authProvider.confirmResetPassword(username, newPassword, code)
+    }
+
+    suspend fun updateEmail(newEmail: String): UpdateEmailResult = mutex.withLock {
+        authProvider.updateEmail(newEmail)
+    }
+
+    suspend fun confirmUpdateEmail(code: String): Result<Unit, AuthError> = mutex.withLock {
+        authProvider.confirmUpdateEmail(code)
+    }
+
+    fun isValidPassword(password: String): Boolean {
+        val lengthRule = password.length >= 8
+        val hasNumber = password.contains(Regex("[0-9]"))
+        val hasUppercase = password.contains(Regex("[A-Z]"))
+        val hasLowercase = password.contains(Regex("[a-z]"))
+        return lengthRule && hasNumber && hasUppercase && hasLowercase
     }
 }
 
